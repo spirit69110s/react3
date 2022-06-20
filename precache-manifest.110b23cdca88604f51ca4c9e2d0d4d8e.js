df095b726127a076649c7264bf40f295f211b2ab
spirit69110s <103171817+spirit69110s@users.noreply.github.com>
2022-06-20 23:33:40 +0430

    Add files via upload

---
+++ precache-manifest.110b23cdca88604f51ca4c9e2d0d4d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de87e16f3c43557cd65e326e72c82724",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44721195caf4616d11ab",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "e5a26e8f56211a948528",
+    "url": "./static/js/2.0938dda7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0938dda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44721195caf4616d11ab",
+    "url": "./static/js/main.19b4f952.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
